09c883f8b45635ed564c029f2f36eb80068e7a54
Maciej <obrebski.maciej@wp.pl>
2022-06-22 22:19:43 +0200

    Normalizing GlobalCSS

--- src/GlobalCSS.ts
+++ src/GlobalCSS.ts
@@ -1,11 +1,24 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  *,*::before,*::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  :root {
+    --background: #299900;
+  }
+
+  html {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 62,5%;
+  }
   body {
     margin: 0;
     padding: 0;
     height: 100vh;
-    background: #333;
+    background: var(--background);
     color: white;
   }
 `
