1608b64e3d15e843afa98b0ec306472c0bb92986
Maciej <obrebski.maciej@wp.pl>
2022-06-23 13:52:04 +0200

    Adding h1 to Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,14 +5,27 @@ import HeroImg from '../../assets/beach_volley_hero.jpg'
 export default function Hero() {
     return (
         <Container style={{ backgroundImage: `url(${HeroImg})` }}>
-
+            <h1>Make your own tournament come true!</h1>
         </Container>
     )
 }
 
 const Container = styled.div`
     height: 60vh;
+    position: relative;
     background-repeat: no-repeat;
     background-position: center center;
     background-size: cover;
+
+    & h1 {
+        position: absolute;
+        top: 70px;
+        left: 50%;
+        transform: translateX(-50%);
+        text-shadow: 0 0 30px #333;
+        font-size: 6rem;
+        color: #fff;
+        font-weight: bold;
+        text-align: center;
+    }
 `
